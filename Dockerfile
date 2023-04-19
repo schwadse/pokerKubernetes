6a78167bd7caf40725dfad37c47b1e52b1a109bc
schwadse <kurz.justin@googlemail.com>
2023-04-19 10:28:30 +0200

    Python app mit Port 1.0.2

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,6 @@ FROM python:3.11
 ADD main.py .
 
 RUN pip install poker
+RUN pip install "fastapi[all]"
 
 CMD [ "python", "./main.py" ]
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from poker import Rank, Card
+from fastapi import FastAPI
+import uvicorn
+import random
+app = FastAPI()
+
+@app.get('/random')
+def root():
+    return {'myCard': random.randint(1, 100)}
 
 
 if __name__ == "__main__":
-    print('Möchtest du eine Randomkarte?')
-    x = input()
-    print(Card.make_random())
+   
+    uvicorn.run(app, host="pythonapp.127.0.0.1.nip.io", port=80)
+
